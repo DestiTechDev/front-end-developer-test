432489f82467a409d9afb149aa068b4967a28c0e
brwnalima <bruna.lima@edu.unirio.br>
2023-03-24 11:05:33 -0300

    card component

--- destinow/src/components/pages/products/Products.jsx
+++ destinow/src/components/pages/products/Products.jsx
@@ -3,8 +3,8 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import './Products.css'
 import Carousel from 'react-bootstrap/Carousel';
-import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
+import Cards from '../../card/Card';
 
 function Products() {
     return (
@@ -13,51 +13,12 @@ function Products() {
 
                 <h1>NOSSOS PACOTES</h1>
                 <Col className="prod-col">
-                    <Carousel className="prod-carousel">
+                    <Carousel variant="dark" className="prod-carousel">
                         <Carousel.Item interval={1000} className="prod-car-item">
                             <CardGroup className="prod-card">
-                                <Card className="prod-card-item">
-                    
-                                    <Card.Img className="prod-card-img" variant="top" src=".\src\assets\card1.png" />
-                                    <Card.Body>
-                                        <Card.Title>Card title</Card.Title>
-                                        <Card.Text>
-                                            This is a wider card with supporting text below as a natural lead-in
-                                            to additional content. This content is a little bit longer.
-                                        </Card.Text>
-                                    </Card.Body>
-                                    <Card.Footer>
-                                        <small className="text-muted">Last updated 3 mins ago</small>
-                                    </Card.Footer>
-                                </Card>
-                                <Card className="prod-card-item">
-                                    <Card.Img className="prod-card-img" variant="top" src="holder.js/100px160" />
-                                    <Card.Body>
-                                        <Card.Title>Card title</Card.Title>
-                                        <Card.Text>
-                                            This card has supporting text below as a natural lead-in to
-                                            additional content.{' '}
-                                        </Card.Text>
-                                    </Card.Body>
-                                    <Card.Footer>
-                                        <small className="text-muted">Last updated 3 mins ago</small>
-                                    </Card.Footer>
-                                </Card>
-
-                                <Card className="prod-card-item">
-                                    <Card.Img className="prod-card-img" variant="top" src="holder.js/100px160" />
-                                    <Card.Body>
-                                        <Card.Title>Card title</Card.Title>
-                                        <Card.Text>
-                                            This is a wider card with supporting text below as a natural lead-in
-                                            to additional content. This card has even longer content than the
-                                            first to show that equal height action.
-                                        </Card.Text>
-                                    </Card.Body>
-                                    <Card.Footer>
-                                        <small className="text-muted">Last updated 3 mins ago</small>
-                                    </Card.Footer>
-                                </Card>
+                                <Cards src=".\src\assets\card1.png" price="R$800,00" />
+                                <Cards src=".\src\assets\card2.png" price="R$640,00"/>
+                                <Cards src=".\src\assets\card3.png" price="R$240,00"/>
                             </CardGroup>
                         </Carousel.Item>
                     </Carousel>
