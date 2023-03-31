2e63cc356faacc303d45d18ced432aa6b65d8f7c
brwnalima <bruna.lima@edu.unirio.br>
2023-03-31 18:11:13 -0300

    responsividade

--- destinow/src/components/form/Form.jsx
+++ destinow/src/components/form/Form.jsx
@@ -4,6 +4,7 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';import './Form.css'
 import { Button } from '../button/Button';
 
+
 function Forms() {
     const [validated, setValidated] = useState(false);
 
@@ -18,37 +19,36 @@ function Forms() {
     };
 
     return (
-        <Form noValidate validated={validated} onSubmit={handleSubmit} className='form-container'>
-            <h3 className='form-h3'>ENTRE EM CONTATO CONOSCO</h3>
-            <Row className="mb-3">
-                <Form.Group className="mb-3" controlId="validationCustom01">
-                    <Form.Control
-                        required
-                        type="text"
-                        placeholder="Seu nome"
-                    />
-                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                </Form.Group>
-            </Row>
-            <Row className="mb-3">
-                <Form.Group as={Col} md="6" controlId="formBasicEmail">
-                    <Form.Control type="email" placeholder="Seu email" />
-                </Form.Group>
+            <Form noValidate validated={validated} onSubmit={handleSubmit} className='form-container'>
+                <h3 className='form-h3'>ENTRE EM CONTATO CONOSCO</h3>
+                <Row className="mb-3">
+                    <Form.Group className="mb-3" controlId="validationCustom01">
+                        <Form.Control
+                            required
+                            type="text"
+                            placeholder="Seu nome"
+                        />
+                        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                    </Form.Group>
+                </Row>
+                <Row className="mb-3">
+                    <Form.Group as={Col} md="6" controlId="formBasicEmail">
+                        <Form.Control type="email" placeholder="Seu email" />
+                    </Form.Group>
+                    
+                    <Form.Group as={Col} md="6" controlId="formBasicTelefone">
+                        <Form.Control type="tel" placeholder="Telefone" />
+                    </Form.Group>
 
-                <Form.Group as={Col} md="6" controlId="formBasicTelefone">
-                    <Form.Control type="tel" placeholder="Telefone" />
-                </Form.Group>
-                
-            </Row>
-            <Row className="mb-3">
-                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                    <Form.Control as="textarea" rows={3} placeholder='Escreva sua mensagem (opcional)' />
-                </Form.Group>
-            </Row>
+                </Row>
+                <Row className="mb-3">
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                        <Form.Control as="textarea" rows={3} placeholder='Escreva sua mensagem (opcional)' />
+                    </Form.Group>
+                </Row>
             
-            <Button type="submit" buttonStyle={'meu-btn--primary-border'} buttonSize={'meu-btn--medium'}>ENVIAR MENSAGEM</Button>
-    
-        </Form>
+                <Button type="submit" buttonStyle={'meu-btn--primary-border'} buttonSize={'meu-btn--medium'}>ENVIAR MENSAGEM</Button>
+            </Form>
     );
 }
 
