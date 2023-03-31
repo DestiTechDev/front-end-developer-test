3a8090d0536fbd5e3c4b51c8b95fe947ee7d518e
brwnalima <bruna.lima@edu.unirio.br>
2023-03-31 16:35:06 -0300

    att

--- destinow/src/components/form/Form.jsx
+++ destinow/src/components/form/Form.jsx
@@ -4,7 +4,7 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Row from 'react-bootstrap/Row';
-import './Form.css'
+// import './Form.css'
 
 function Forms() {
     const [validated, setValidated] = useState(false);
