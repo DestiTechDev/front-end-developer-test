de93ab04a83a1739e996a22eeabc8e79d74471fe
Brunno Costa <brunno.costa.su@outlook.com>
2023-10-18 12:44:10 -0300

    travels section included

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import './App.css'
 import Header from './components/Header/Header'
-import Carousel from './components/Carousel/carousel'
+import Carousel from './components/Carousel/Carousel'
+import TravelCarousel from './components/TravelCarousel/TravelCarousel'
 
 const App = () => {
   return (
     <div className="App">
       <Header/>
       <Carousel/>
+
+
+      <div className='flex justify-center flex-col items-center mt-8'>
+        <h2 className="uppercase font-bold p-3 text-xl">nossos pacotes</h2>
+        <div className='border-b border-blue-500 w-8 border-2'></div>
+      </div>
+      <TravelCarousel/>
     </div>
   )
 }
