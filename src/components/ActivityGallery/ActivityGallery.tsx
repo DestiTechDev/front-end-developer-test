0914d7bb70eaf4c72d57c0b335c53707924fcd61
Brunno Costa <brunno.costa.su@outlook.com>
2023-10-19 16:02:08 -0300

    motion animation applied

--- src/components/ActivityGallery/ActivityGallery.tsx
+++ src/components/ActivityGallery/ActivityGallery.tsx
@@ -4,6 +4,8 @@ import image2 from "../../images/Rectangle_24_act.png";
 import image3 from "../../images/Rectangle_25_act.png";
 import image4 from "../../images/Rectangle_26_act.png";
 import SectionTitle from '../SectionTitle/SectionTitle';
+import { motion } from 'framer-motion';
+import { useInView } from "react-intersection-observer";
 
 const activities = [
     {
@@ -26,11 +28,19 @@ const activities = [
   
 
 const ActivityGallery = () => {
+  const [ref, inView] = useInView({
+    triggerOnce: false,
+  });
+  
   return (
     <>
       <SectionTitle title="Nossas atividades"/>
       <div className='max-w-screen-xl mx-auto py-8 px-8'>
-        <div className='grid grid-cols-1 sm:grid-cols-2 gap-6 justify-center'>
+        <motion.div className='grid grid-cols-1 sm:grid-cols-2 gap-6 justify-center'
+            initial={{ opacity: 0, x: -200 }}
+            animate={inView ? { opacity: 1, x: 0 } : { opacity: 0, x: -80 }}
+            transition={{ duration: 1 }}
+            ref={ref}>
           {
             activities.map(activity => (
               <div key={activity.id} className="relative overflow-hidden cursor-pointer">
@@ -43,7 +53,7 @@ const ActivityGallery = () => {
               </div>
             ))
           }
-        </div>
+       </motion.div>
       </div>
     </>
   )
--- src/components/Infos/Infos.tsx
+++ src/components/Infos/Infos.tsx
@@ -1,53 +1,67 @@
-import React from 'react'
-import image1 from "../../images/home.png"
+import React from "react";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import image1 from "../../images/home.png";
 import image2 from "../../images/brand.png";
 import image3 from "../../images/gifts.png";
 
-
 const infos = [
-    {
-      id: "1",
-      thumb: image1,
-      title: "MAIS DE 20 LOJAS",
-      subtitle: "Contrary to popular belief, Lorem Ipsum is not simply random text."
-    },
-    {
-      id: "2",
-      thumb: image2,
-      title: "SEGURANÇA",
-      subtitle: "Contrary to popular belief, Lorem Ipsum is not simply random text."
-    },
-    {
-      id: "3",
-      thumb: image3,
-      title: "OS MELHORES PREÇOS",
-      subtitle: "Contrary to popular belief, Lorem Ipsum is not simply random text."
-    },
+  {
+    id: "1",
+    thumb: image1,
+    title: "MAIS DE 20 LOJAS",
+    subtitle:
+      "Contrary to popular belief, Lorem Ipsum is not simply random text.",
+  },
+  {
+    id: "2",
+    thumb: image2,
+    title: "SEGURANÇA",
+    subtitle:
+      "Contrary to popular belief, Lorem Ipsum is not simply random text.",
+  },
+  {
+    id: "3",
+    thumb: image3,
+    title: "OS MELHORES PREÇOS",
+    subtitle:
+      "Contrary to popular belief, Lorem Ipsum is not simply random text.",
+  },
+];
 
-  ];
-  
+const Infos = () => {
+  const [ref, inView] = useInView({
+    triggerOnce: false,
+  });
 
-const Infos = () =>{
   return (
-    <div className='max-w-screen-xl mx-auto pt-16 pb-16'>
-      <div className='flex flex-wrap justify-center'>
+    <div className="max-w-screen-xl mx-auto pt-16 pb-16">
+      <div className="flex flex-wrap justify-center">
         {infos.map((info) => (
           <div
             key={info.id}
             className="w-[320px] flex flex-col items-center sm:flex-row lg:flex-row sm:items-center lg:items-center mx-12 my-4"
           >
+            <motion.div
+              key={info.id}
+              className="w-[320px] flex flex-col items-center sm:flex-row lg:flex-row sm:items-center lg:items-center mx-12 my-4"
+              initial={{ opacity: 0, x: -200 }}
+              animate={inView ? { opacity: 1, x: 0 } : { opacity: 0, x: -80 }}
+              transition={{ duration: 1 }}
+              ref={ref}
+            >
             <img src={info.thumb} width={70} height={70} alt={`brand-${info.id}`} />
-            <div className="w-[300px] mt-2 lg:ml-2 sm:ml-0 lg:text-left sm:text-center ">
-              <p className="font-bold uppercase text-[20px]">{info.title}</p>
-              <p className="text-[16px]">{info.subtitle}</p>
-            </div>
+
+              <div className="w-[300px] mt-2 lg:ml-2 sm:ml-0 lg:text-left sm:text-center">
+                <p className="font-bold uppercase text-[20px]">{info.title}</p>
+                <p className="text-[16px]">{info.subtitle}</p>
+              </div>
+            </motion.div>
           </div>
         ))}
       </div>
     </div>
   );
-  
-  
-}
+};
 
-export default Infos
+export default Infos;
