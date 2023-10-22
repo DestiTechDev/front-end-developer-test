bc0baa59b3033c6d8aaa986f7dc7ec91396ee8c2
leonardomeneguell1 <leonardomeneguelli@hotmail.com>
2023-10-22 15:21:36 -0300

    Consonantização dos Cards e Ajuste de Estrutura de pastas

---
+++ excursion/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import NavbarTab from "./components/navbar/Navbar";
+import Header from "./components/header/Header";
+
+function App() {
+
+  return (
+    <div className="App">
+      <NavbarTab/>
+      <Header/>
+    </div>
+  )
+}
+
+export default App
